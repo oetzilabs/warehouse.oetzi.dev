6ed9a4d1ef75183fb4450eead62c0e6f76cd75f6
Özgür Isbert <oezguerisbert@gmail.com>
2025-04-30 00:00:47 +0200

    wip

--- packages/web/src/components/providers/Authentication.tsx
+++ packages/web/src/components/providers/Authentication.tsx
@@ -1,15 +1,15 @@
 import { createSignal } from "solid-js";
 import { isServer } from "solid-js/web";
 // import { setCookie, getCookie } from "vinxi/http";
-import { z } from "zod";
+import { InferInput, object, string } from "valibot";
 
-export const UserSchema = z.object({
-  id: z.string(),
-  username: z.string(),
-  image: z.string(),
-  email: z.string(),
+export const UserSchema = object({
+  id: string(),
+  username: string(),
+  image: string(),
+  email: string(),
 });
 
 export const [authLoggedin, setAuthLoggedin] = createSignal<boolean>(false);
 
-export const [auth, setAuth] = createSignal<z.infer<typeof UserSchema> | null>(null);
+export const [auth, setAuth] = createSignal<InferInput<typeof UserSchema> | null>(null);
