45930cd79416b3bd7b04179dae6290ba1ca21c19
Özgür Isbert <oezguerisbert@gmail.com>
2025-05-08 19:44:18 +0200

    deps, chores

--- packages/web/src/routes/(app)/(layout)/map.tsx
+++ packages/web/src/routes/(app)/(layout)/map.tsx
@@ -58,8 +58,8 @@ export default function DashboardPage() {
   onMount(() => {
     setBreadcrumbs([
       {
-        label: "Dashboard",
-        href: "/dashboard",
+        label: "Map",
+        href: "/map",
       },
     ]);
   });
