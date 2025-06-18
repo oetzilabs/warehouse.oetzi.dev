fa5749119a2d59d6a29e3a8d2da8ac5be0c1dd86
Özgür Isbert <oezguerisbert@gmail.com>
2025-06-18 10:37:09 +0200

    wip: realtime feature

--- packages/realtime/src/index.ts
+++ packages/realtime/src/index.ts
@@ -1,10 +1,25 @@
 import { BunRuntime } from "@effect/platform-bun";
-import { Effect } from "effect";
+import { Config, Effect, Schema } from "effect";
 import { program } from "./program";
+import { createEventHandler } from "./types";
 
-const brokerUrl = process.env.BROKER_URL!;
-const clientId = process.env.CLIENT_ID!;
-
-BunRuntime.runMain(Effect.scoped(program(brokerUrl, clientId)));
+BunRuntime.runMain(
+  Effect.scoped(
+    Effect.gen(function* () {
+      const brokerUrl = yield* Config.string("BROKER_URL");
+      const clientId = yield* Config.string("CLIENT_ID");
+      return program(brokerUrl, clientId, [
+        createEventHandler({
+          channel: "realtime/:any",
+          schema: Schema.String,
+          handle: (topic, params, data) =>
+            Effect.gen(function* () {
+              yield* Effect.log("Received message for topic", topic, params, data);
+            }),
+        }),
+      ]);
+    }),
+  ),
+);
 
 export {};
--- packages/realtime/src/program.ts
+++ packages/realtime/src/program.ts
@@ -1,23 +1,12 @@
 import { Effect, Schema } from "effect";
 import { MQTTLive, MQTTService } from "./services/mqtt";
-import { createEventHandler } from "./types";
+import { createEventHandler, InfallibleEventHandler } from "./types";
 
-export const program = (brokerUrl: string, clientId: string) =>
+export const program = <A, I>(brokerUrl: string, clientId: string, handlers: InfallibleEventHandler<A, I>[]) =>
   Effect.gen(function* (_) {
     const mqtt = yield* _(MQTTService);
 
     yield* Effect.acquireRelease(mqtt.connect(brokerUrl, clientId), (client, exit) => mqtt.disconnect(client));
-
-    const handlers = [
-      createEventHandler({
-        channel: "realtime/:any",
-        schema: Schema.String,
-        handle: (topic, params, data) =>
-          Effect.gen(function* () {
-            yield* Effect.log("Received message for topic", topic, params, data);
-          }),
-      }),
-    ];
 
     const unsubscribers = yield* mqtt.subscribe(handlers);
 
