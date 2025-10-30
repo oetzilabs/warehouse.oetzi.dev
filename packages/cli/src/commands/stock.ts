0ebb5a8f5b07f748b89a5cf5c6342504072e5f3a
Özgür Isbert <oezguerisbert@gmail.com>
2025-10-30 10:57:21 +0100

    fix: update drizzle-orm version to 0.44.7 in package.json and bun.lock refactor: remove printer service from stock command and its related functionality

--- packages/cli/src/commands/stock.ts
+++ packages/cli/src/commands/stock.ts
@@ -1,11 +1,10 @@
 import { Args, Command, Options } from "@effect/cli";
 import { InventoryLive, InventoryService } from "@warehouseoetzidev/core/src/entities/inventory";
 import { OrganizationId } from "@warehouseoetzidev/core/src/entities/organizations/id";
-import { PrinterLive, PrinterService } from "@warehouseoetzidev/printer/src/index";
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import { Console, Effect, Layer } from "effect";
-import { formatOption, keysOption, orgOption, output, printOption } from "./shared";
+import { formatOption, keysOption, orgOption, output } from "./shared";
 
 dayjs.extend(localizedFormat);
 
@@ -13,48 +12,14 @@ const storageOption = Options.text("storage").pipe(Options.withDescription("The 
 
 const stockCommand = Command.make(
   "stock",
-  { org: orgOption, format: formatOption, keys: keysOption, print: printOption },
-  Effect.fn("@warehouse/cli/stock.show")(function* ({ org, format, keys, print }) {
+  { org: orgOption, format: formatOption, keys: keysOption },
+  Effect.fn("@warehouse/cli/stock.show")(function* ({ org, format, keys }) {
     const repo = yield* InventoryService;
     const stats = yield* repo.statistics().pipe(Effect.provide(Layer.succeed(OrganizationId, org)));
-    if (print) {
-      const printer = yield* PrinterService;
-      yield* printer.print(printer.usb(), {
-        text: [
-          {
-            content: "Inventory Statistics",
-            font: "a",
-            align: "ct",
-            style: "b",
-            size: [2, 2],
-          },
-          {
-            content: `Capacity: ${stats.capacity}`,
-            font: "a",
-            align: "ct",
-            style: "b",
-            size: [2, 2],
-          },
-          {
-            content: `Storages: ${stats.storages.length}`,
-            font: "a",
-            align: "ct",
-            style: "b",
-            size: [2, 2],
-          },
-          // stats.storages.map((s) => ({
-          //   content: `Storage: ${s.name}`,
-          //   font: "a",
-          //   align: "ct",
-          //   style: "b",
-          //   size: [2, 2],
-          // })),
-        ],
-      });
-    }
+
     return yield* output(stats, format, keys);
   }),
 );
 
 export default stockCommand;
-export const layers = Layer.mergeAll(InventoryLive, PrinterLive);
+export const layers = Layer.mergeAll(InventoryLive);
